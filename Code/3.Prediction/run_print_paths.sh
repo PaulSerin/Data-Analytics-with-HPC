47a0893c5e1943696f24eb6fbd7bd3f08baa90a7
Paul SERIN <serinpaul1@gmail.com>
2025-05-19 23:44:00 +0200

    add sh

--- Code/3.Prediction/run_print_paths.sh
+++ Code/3.Prediction/run_print_paths.sh
@@ -2,11 +2,11 @@
 #SBATCH --time=00:05:00
 #SBATCH --mem=4G
 
-#SBATCH --chdir=/mnt/netapp2/Store_uni/home/ulc/cursos/curso363/TFM/Data-Analytics-with-HPC/Code/3.Prediction
-
 # Optional: load modules or activate your environment
 # module load python/3.9
 
 source /path/to/venv/bin/activate
 
-srun python print_paths.py
+module load python/3.9
+      # or whatever module you need
+python3 print_paths.py
