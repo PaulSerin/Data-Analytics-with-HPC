9378e07b03ac7eb77d4dd9f83751abca9910e250
Paul SERIN <serinpaul1@gmail.com>
2025-06-04 20:37:49 +0200

    bash for sequential and distributed montecarlo simulation

--- Code/4.Prediction/RG_2025/monte_carlo_rg_seq.sh
+++ Code/4.Prediction/RG_2025/monte_carlo_rg_seq.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
-#SBATCH --job-name=mc-rg-distributed
-#SBATCH --output=logs/mc-rg-distributed-%A_%a.out
-#SBATCH --error=logs/mc-rg2--distributed-%A_%a.err
-#SBATCH --array=0-3                # 4 tasks: IDs 0 and 3
+#SBATCH --job-name=mc-rg-sequential
+#SBATCH --output=logs/mc-rg-seq-%A_%a.out
+#SBATCH --error=logs/mc-rg-seq-%A_%a.err
+#SBATCH --array=0-0                
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32         # 32 CPU cores per GPU (cluster policy)
@@ -14,7 +14,7 @@
 module load python
 source $STORE/mypython/bin/activate
 
-RUNS_PER_JOB=250                    # 250 simulations each
+RUNS_PER_JOB=1000                    # 1000 simulations each
 SCRIPT=monte_carlo_rg.py
 
 echo "[$(date)] Starting job $SLURM_JOB_ID task $SLURM_ARRAY_TASK_ID"
@@ -26,6 +26,6 @@ python -u $SCRIPT \
     --cutoff        2025-01-01 \
     --runs-per-job  $RUNS_PER_JOB \
     --job-index     $SLURM_ARRAY_TASK_ID \
-    --output-dir    ./mc_rg_results
+    --output-dir    ./mc_rg_results_seq
 
 echo "[$(date)] Finished job $SLURM_JOB_ID task $SLURM_ARRAY_TASK_ID"
